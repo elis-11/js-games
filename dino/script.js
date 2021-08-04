05607e43c92a37e9e46fcb56e606617c7e156e4e
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-08-04 12:49:48 +0200

    if(cactusLeft<50 && cactusLeft>0&&dinoTop>=140){

--- dino/script.js
+++ dino/script.js
@@ -5,11 +5,20 @@ document.addEventListener("keydown", function (event) {
   jump();
 });
 
-jump = () => {
+function jump() {
   if (dino.classList != "jump") {
     dino.classlist.add("jump");
   }
   setTimeout(function () {
     dino.classList.remove("jump");
   }, 300);
-};
+}
+let isAlive = setInterval(function () {
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+  if(cactusLeft<50 && cactusLeft>0&&dinoTop>=140){
+    alert("CAME OVER!")
+  }
+}10);
