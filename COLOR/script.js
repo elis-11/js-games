b1171bc2e1840a79377e99287e9ab2bad70cc909
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-08-04 21:15:50 +0200

    ######VERSION-1########

--- COLOR/script.js
+++ COLOR/script.js
@@ -1,7 +1,32 @@
-// const button = document.createElementById('btn')
-// const color=document.querySelector('.color')
+const button = document.getElementById("btn");
+const color = document.querySelector(".color");
 
-// const hex=[
-//     '0', '1', '2', '3', '4', '5','6', '7', '8', '9', 'A','B','C', 'D', 'E', 'F'
-// ]
+// ######VERSION-1########
+const colors = ["red", "green", "yellow", "orange", "black", "white"];
+button.addEventListener("click", () => {
+  let hexColor = colors[getRandomNumber()];
+  document.body.style.backgroundColor=hexColor;
+  color.textContent=hexColor;
+});
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length)
+}
 
+// const hex = [
+//   "0",
+//   "1",
+//   "2",
+//   "3",
+//   "4",
+//   "5",
+//   "6",
+//   "7",
+//   "8",
+//   "9",
+//   "A",
+//   "B",
+//   "C",
+//   "D",
+//   "E",
+//   "F",
+// ];
