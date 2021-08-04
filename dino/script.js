05fc2d410c5f4f7615504cf396fb373a3dd6ff31
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-08-04 12:08:23 +0200

    animation: cactusMov 1s infinite linear;

--- dino/script.js
+++ dino/script.js
@@ -1,6 +1,8 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 
-document.addEventListener('keydown', function(event) {
-    jump()
-})
+document.addEventListener("keydown", function (event) {
+  jump();
+});
+
+const jump = () => {};
